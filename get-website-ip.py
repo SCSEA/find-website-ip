86ac755c3b3b6a2e8f12614db4470c6a0ac04cd0
SCSEA <138138604+SCSEA@users.noreply.github.com>
2024-03-31 17:45:30 +0300

    Update get-website-ip.py

--- get-website-ip.py
+++ get-website-ip.py
@@ -1,34 +1,94 @@
 import os
-os.system('pip install socket')
-os.system('clear')
+import socket
+import time
+import sys
+import pyfiglet
+import colorama
+from colorama import Fore, init
 
-print("[+] Tool Name:Find-Website-IP")
-print("[+] Author:Yousuf Shafi'i Muhammad.Junior Programmer")
-print("[+] Version:1.0")
-print("[+] Team:Junior Programmers")
-print("[+] Github:https://github.com/Yousuf9963/phone-num-info")
-print("[+] Follow me on Github: https://github.com/Yousuf9963")
-print("[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog.")
-print("[!] legal disclaimer: Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
-print("[+] I hope for you good future and i am willing that you will come high effort.")
+try:
+    import requests
+except ModuleNotFoundError:
+    os.system("pip install requests")
+    os.system("pip install socket")
+    os.system("pip install time")
+    os.system("pip install colorama")
+    os.system("pip install pyfiglet")
+
+# Clear the console screen
+os.system("cls" if os.name == "nt" else "clear")
+
+init(autoreset=True)
+
+# Delay for displaying messages
+delay = 7
+
+# Legal disclaimer
+print(Fore.BLUE + "[!] Legal Disclaimer : Usage of this Program for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.")
+print("")
+print(Fore.BLUE + "[+] I hope for you good future and i am willing that you will come high effort.")
+time.sleep(delay)
+print("")
+
+# Print ASCII art
+fg = pyfiglet.Figlet(font="standard").renderText("Find-website-ip")
+print(Fore.CYAN + fg)
+print("")
+
+
+
+def banner():
+
+    print(Fore.LIGHTBLUE_EX + """
+
+___________________________________________________________________________
+
+                                                                                                                      >
+
+                   <Program>  Find Website IP  </Program>
+
+
+
+                 <Developer>  Yousuf Shafii Muhammad  </Developer>
+
+
+
+            <GitHub> https://github.com/SCSEA </GitHub>
+
+
+
+           <Tiktok> tiktok.com/@programmer_boy_2 </Tiktok>
+
+
+
+___________________________________________________________________________
+
+
+
+    """)
+
+banner()
+
+
+# Input target website
+url = input(Fore.BLUE + "Enter Target Website: ")
+print("")
+time.sleep(1)
+print(Fore.GREEN + "Getting IP Address Attacking &&& Accessing &&& Injecting 😀.......")
+time.sleep(2)
+
+# Simulate progress
+for i in range(0, 110, 10):
+    print(Fore.GREEN + f"Getting IP Address {i}%")
+    time.sleep(2)
+
+# Get and print IP address
+try:
+    ip_address = socket.gethostbyname(url)
+    print(Fore.GREEN + "IP Address Found Successfully")
+    print(Fore.GREEN + "IP Address is:", ip_address)
+except socket.gaierror:
+    print(Fore.RED + "Failed to resolve the host")
 
 print("")
-import socket
-
-url = input("Enter Target Website:");
-
-print("Getting IP Address attacking........")
-print("Getting IP Address  0%")
-print("Getting IP Address  10%")
-print("Getting IP Address  20%")
-print("Getting IP Address  40%")
-print("Getting IP Address  60%")
-print("Getting IP Address  70%")
-print("Getting IP Address  80%")
-print("Getting IP Address  90%")
-print("Getting IP Address  100%")
-print("IP Address Founded Successsfully")
-
-print("IP Address is:" ,socket.gethostbyname(url))
-
 
